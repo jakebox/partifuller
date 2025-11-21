32c8f244b80ed9114460cbe0e16380b10412204e
Jacob Boxerman <jacobboxerman@gmail.com>
2025-11-21 17:38:59 -0500

    add: rsvp_add endpoint

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
 use axum::{
-    Router, extract::State, http::StatusCode, response::{Html}, routing::get
+    Json, Router,
+    extract::State,
+    http::StatusCode,
+    response::Html,
+    routing::{get, post},
 };
 
 use askama::Template;
@@ -23,14 +27,38 @@ struct Home {
     rsvps: Vec<Rsvp>,
 }
 
-#[derive(sqlx::FromRow)]
-struct Rsvp {
-    name: String,
+#[derive(Template)]
+#[template(
+    ext = "html",
+    source = r#"
+<ul>
+{% for rsvp in rsvps %}
+  <li>{{ rsvp.name }}</li>
+{% endfor %}
+</ul>
+"#
+)]
+struct RsvpList {
+    rsvps: Vec<Rsvp>,
 }
 
-async fn index(
-    State(pool): State<SqlitePool>,
-) -> Result<Html<String>, StatusCode> {
+#[derive(sqlx::FromRow)]
+struct Rsvp {
+    id: i64,
+    name: String,
+    email: String,
+    timestamp: i64,
+    attending: i64,
+}
+
+#[derive(serde::Deserialize)]
+struct RsvpNew {
+    name: String,
+    email: String,
+    attending: i64,
+}
+
+async fn index(State(pool): State<SqlitePool>) -> Result<Html<String>, StatusCode> {
     let rsvps = sqlx::query_as("SELECT name from rsvps")
         .fetch_all(&pool)
         .await
@@ -43,12 +71,40 @@ async fn index(
     Ok(Html(page))
 }
 
+async fn add_rsvp(
+    State(pool): State<SqlitePool>,
+    Json(rsvp): Json<RsvpNew>,
+) -> Result<Html<String>, StatusCode> {
+    let _result =
+        sqlx::query("INSERT INTO rsvps (name, email, attending) VALUES (?, ?, ?) RETURNING *")
+            .bind(rsvp.name)
+            .bind(rsvp.email)
+            .bind(rsvp.attending)
+            .fetch_one(&pool)
+            .await
+            .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?;
+
+    let rsvps = sqlx::query_as("SELECT name from rsvps")
+        .fetch_all(&pool)
+        .await
+        .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?;
+
+    let rsvp_html = RsvpList { rsvps }
+        .render()
+        .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?;
+
+    Ok(Html(rsvp_html))
+}
+
 #[tokio::main]
-async fn main()  -> Result<(), sqlx::Error> {
+async fn main() -> Result<(), sqlx::Error> {
     let pool = SqlitePool::connect("sqlite:./partifuller.db").await?;
 
     // build our application with a single route
-    let app = Router::new().route("/", get(index)).with_state(pool);
+    let app = Router::new()
+        .route("/", get(index))
+        .route("/rsvp", post(add_rsvp))
+        .with_state(pool);
 
     // run our app with hyper, listening globally on port 3000
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
